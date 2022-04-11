8dc63396e86af1154f0f2bc132f708642f0984b9
si <si.sijun@outlook.com>
2022-04-11 10:11:21 -1000

    Likes Vs Dislikes -- completed

---
+++ js/solution0051.js
@@ -0,0 +1,33 @@
+/*
+Least Larger
+
+DESCRIPTION:
+Task
+Given an array of numbers and an index, return either the index of the smallest number that is larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+
+Notes
+Multiple correct answers may be possible. In this case, return any one of them.
+The given index will be inside the given array.
+The given array will, therefore, never be empty.
+
+Example
+leastLarger( [4, 1, 3, 5, 6], 0 )  =>  3
+leastLarger( [4, 1, 3, 5, 6], 4 )  => -1
+
+*/
+
+
+//JavaScript Solution1 : 
+
+const leastLarger = (a, i) => a.indexOf(Math.min(...a.filter(n => n > a[i])))
+
+
+//JavaScript Solution2 : 
+
+function leastLarger(a, i) {
+    const targetVal = a[i]
+    const largerVals = a.filter(num => num > targetVal)
+    const leastLargeVal = Math.min(...largerVals)
+    
+    return a.findIndex(num => num === leastLargeVal)
+}
