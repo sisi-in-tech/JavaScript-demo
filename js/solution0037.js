8eff5398498af9c161e4d4965644219386132b7a
si <si.sijun@outlook.com>
2022-04-19 07:35:47 -1000

    Fake Binary --completed

---
+++ js/solution0037.js
@@ -0,0 +1,14 @@
+/*description 
+
+MakeUpperCase
+
+
+Write a function which converts the input string to uppercase.
+
+*/ 
+
+//JavaScript solution : 
+
+function makeUpperCase(str) {
+    return str.toUpperCase() 
+ }
