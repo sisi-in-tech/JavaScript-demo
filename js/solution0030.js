998605cda2514d170f57f57fccda48ca924e1741
si <si.sijun@outlook.com>
2022-06-05 07:20:19 -1000

    Sum of two lowest positive integers--completed

---
+++ js/solution0030.js
@@ -0,0 +1,32 @@
+/*description 
+
+Sum of two lowest positive integers
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+*/
+
+
+//JavaScript solution1 : 
+
+function sumTwoSmallestNumbers(numbers) {  
+    numbers.sort((a,b) => a - b);
+    return numbers[0] + numbers[1];
+  };
+  
+  
+  //JavaScript solution2 : 
+  
+  function sumTwoSmallestNumbers(numbers) {  
+    let first = Math.min(...numbers)
+    numbers.splice(numbers.indexOf(first), 1)
+    let second = Math.min(...numbers)
+    return first + second
+  }
+  
+  
+  
