24fff05f5746f2d98e52543e475ff86ff70315af
si <si.sijun@outlook.com>
2022-06-12 14:42:29 -0700

    Replace With Alphabet Position -- completed

---
+++ js/solution0039.js
@@ -0,0 +1,16 @@
+/*description 
+
+Fake Binary
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+*/ 
+
+// JavaScript solution 
+
+function fakeBin(x) {
+    return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
+
