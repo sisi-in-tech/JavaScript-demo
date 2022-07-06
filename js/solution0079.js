16a7c3a2d7f2e708c34acd127e3d8962bb2fd500
si <si.sijun@outlook.com>
2022-07-05 21:38:07 -0800

    Compare within margin--completed

---
+++ js/solution0079.js
@@ -0,0 +1,17 @@
+/*description 
+Find Nearest square number
+Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+Goodluck :)
+
+Check my other katas:
+
+Alphabetically ordered
+
+Case-sensitive!
+
+Not prime numbers
+*/ 
+
+//JavaScript solution 
+const nearestSq = n => Math.pow(Math.round(Math.sqrt(n)), 2);
