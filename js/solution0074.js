cc5472d5137c9bc083647f37c0ed9643648ff1ae
si <si.sijun@outlook.com>
2022-07-12 22:57:26 -0700

    Will there be enough space?--completed

---
+++ js/solution0074.js
@@ -0,0 +1,11 @@
+/*description 
+Convert a Boolean to a String
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/ 
+//JavaScript solution : 
+function booleanToString(b){
+    return b.toString();
+  }
