59e147f502de01962cbd281419d63cc637b87a99
si <si.sijun@outlook.com>
2022-08-21 08:15:36 -1000

    You only need one - Beginner--completed

---
+++ js/solution0126.js
@@ -0,0 +1,26 @@
+/*
+You only need one - Beginner
+DESCRIPTION:
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+
+*/
+
+
+// JavaScript solution : 
+const check = (a,x) => a.includes(x)? true : false 
+
+
+/* use JavaScript Array includes()
+
+Definition and Usage
+
+The includes() method returns true if an array contains a specified value.
+
+The includes() method returns false if the value is not found.
+
+The includes() method is case sensitive.
+*/
