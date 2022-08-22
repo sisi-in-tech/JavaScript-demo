8b4483b48829be4df5825ada35102fdb99c65994
si <si.sijun@outlook.com>
2022-08-22 11:02:30 -0700

    Function 1 - hello world--completed

--- js/solution0124.js
+++ js/solution0124.js
@@ -1,4 +1,6 @@
 /*
+Currying functions: multiply all elements in an array
+
 To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
 
 The returned array should consist of each of the elements from the first array multiplied by the integer.
