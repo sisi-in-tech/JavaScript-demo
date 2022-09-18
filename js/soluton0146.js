8f3bcd087818b7ee65e0075983006a233edb115a
si <si.sijun@outlook.com>
2022-09-18 02:39:33 -0700

    squaring an argument--completed

---
+++ js/soluton0146.js
@@ -0,0 +1,19 @@
+/*
+Function 2 - squaring an argument
+
+Now you have to write a function that takes an argument and returns the square of it.
+
+
+*/
+
+
+//JavaScript soluton : 
+var square = function (n){
+    return n*n
+  }
+
+//JavaScript soluton : 
+  var square = (n)=> n*n
+
+
+
