b7be6ea0819e07088dd8e15f0b48c6d8975521fc
si <si.sijun@outlook.com>
2022-09-19 15:24:41 -0700

    Climbing Stairs--completed

--- js/solution0145.js
+++ js/solution0145.js
@@ -58,3 +58,13 @@ var recur = function (n, lookup){
     return result ;
 }
 
+var climbStairs =function(n){
+    const memo = [1, 1, 2] //inital value 1, 1, 2 
+    for (let i = 3 ; i <=n ; i++){
+        memo[i]=memo[i-1] + memo[i-2];
+    }
+    return memo[n]
+}
+
+
+
